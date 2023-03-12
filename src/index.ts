62285821c33b10ffc12f4d30f173190ad3fc30e1
TheDogHusky <leredlabo@gmail.com>
2023-03-12 17:50:40 +0100

    Added images directory (changed .gitignore)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import App from './structures/app';
+import fs from 'fs';
 
 const app = new App(3000);
 app.listen();
+
+if(!fs.existsSync('./config.ts')) {
+    fs.copyFileSync('./config.example.ts', './config.ts');
+}
+
+if(!fs.existsSync('./images')) {
+    fs.mkdirSync('./images');
+}
+
+if(!fs.existsSync('./logs')) {
+    fs.mkdirSync('./logs');
+}
