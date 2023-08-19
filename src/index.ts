e7eda7c4872e1f9829bbea9e1548572b062c1e5c
Adam <classy.craft3r@gmail.com>
2023-08-19 15:25:15 +0200

    Add support for slaps

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import App from './structures/app';
 import fs from 'fs';
 import * as config from './config';
 
-const app = new App(config.port);
-app.listen();
-
 if(!fs.existsSync('./images')) {
     fs.mkdirSync('./images');
 }
@@ -12,3 +9,6 @@ if(!fs.existsSync('./images')) {
 if(!fs.existsSync('./logs')) {
     fs.mkdirSync('./logs');
 }
+
+const app = new App(config.port);
+app.listen();
